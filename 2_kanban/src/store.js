3363d3c50b8594d80f01d542159d28c471ae61e6
Sumukha2000 <57064739+Sumukha2000@users.noreply.github.com>
2023-11-09 18:47:17 +0530

    Delete 2_kanban directory

--- 2_kanban/src/store.js
+++
@@ -1,7 +0,0 @@
-import { create } from "zustand";
-const store = (set) => ({
-  tasks: [{ title: "Test task", state: "PLANNED" }], 
-  addTask:(title,state)=>set((store)=>({tasks:[...store.tasks,{title,state}]}))
-});
-
-export const useStore = create(store);
